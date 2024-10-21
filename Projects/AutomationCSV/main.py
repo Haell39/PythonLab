e4714fbb843fb8604c6727061ee194c63abd95c5
Rael <santosrafaelpro@gmail.com>
2024-10-20 22:11:29 -0300

    Update main.py

--- Projects/AutomationCSV/main.py
+++ Projects/AutomationCSV/main.py
@@ -1,6 +1,6 @@
 # çç Instructions:
 
-'''
+"""
 1. Create/Import a CSV file to VSCode
 2. Open browser
 3. Access the system site with loguin and password
@@ -13,13 +13,15 @@
 # Passo 3: Importar a base de produtos pra cadastrar
 # Passo 4: Cadastrar um produto
 # Passo 5: Repetir o processo de cadastro até o fim
-'''
+"""
 
 import pyautogui
 import time
 import pandas as pd
+import keyboard
 
 # Importing the CSV file
+# Passo 3: Importar a base de produtos pra cadastrar
 table = pd.read_csv("Projects/AutomationCSV/produtos.csv")
 print(table)
 
@@ -28,7 +30,7 @@ pyautogui.PAUSE = 0.6
 
 # abrir sistemas (Brave)
 pyautogui.press("win")
-pyautogui.write("brave")
+pyautogui.write("firefox")
 pyautogui.press("enter")
 time.sleep(3)
 pyautogui.hotkey("ctrl", "t")
@@ -40,4 +42,40 @@ pyautogui.press("enter")
 time.sleep(5)
 
 # loguin in the system
+posicao = pyautogui.position()
+time.sleep(5)
+print(posicao)
 
+pyautogui.click(x=1354, y=343)
+pyautogui.write("IqZCZ@example.com")
+pyautogui.press("tab")
+pyautogui.click(x=1435, y=486)
+
+
+# Passo 4: Cadastrar um produto
+
+for line in table.index:
+    # parando a automação:
+    if keyboard.is_pressed("q"):
+        print("Ending program...")
+        break
+    pyautogui.click(x=1810, y=45)
+    pyautogui.write(str(table.loc[line, "codigo"]))
+    pyautogui.press("tab")
+    pyautogui.write(str(table.loc[line, "marca"]))
+    pyautogui.press("tab")
+    pyautogui.write(str(table.loc[line, "tipo"]))
+    pyautogui.press("tab")
+    pyautogui.write(str(table.loc[line, "categoqMOLO000251  ria"]))
+    pyautogui.press("tab")
+    pyautogui.write(str(table.loc[line, "preco_unitario"]))
+    pyautogui.press("tab")
+    pyautogui.write(str(table.loc[line, "custo"]))
+    pyautogui.press("tab")
+    if not pd.isna(table.loc[line, "obs"]):
+        pyautogui.write(str(table.loc[line, "obs"]))
+    pyautogui.click(x=1805, y=53)
+    pyautogui.scroll(5000)
+
+
+# Passo 5: Repetir o processo de cadastro até o fim
