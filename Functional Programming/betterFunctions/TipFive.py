83e8fa7fed0782dddfa4c4c9f4dcedfae0777dfc
Rael <santosrafaelpro@gmail.com>
2024-10-21 11:30:15 -0300

    Update TipFive.py

---
+++ Functional Programming/betterFunctions/TipFive.py
@@ -0,0 +1,24 @@
+
+'''
+Essa nao é uma ideia sustentavel pois nao da para passar um valor 'D' por exemplo ou excluir o valor 'A' e só passar 'B' e 'C' pois teria que ficar modificando os parametros de def join_text
+
+def join_text(text1: str, text2: str, text3: str, *, sep: str) -> str:
+    return sep.join([text1, text2, text3])
+
+def main() -> None:
+    print(join_text('A', 'B', 'C', sep='--'))
+
+if __name__ == '__main__':
+    main()
+'''
+
+def join_text(*strings, sep: str) -> str:
+    return sep.join(strings)
+
+def main() -> None:
+    print(join_text( 'B', 'C', sep='--'))
+    print(join_text('A', 'G', 'K', 'L', 'H', sep='/'))
+
+if __name__ == '__main__':
+    main()
+
