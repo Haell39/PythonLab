a1d2b3cb71a28fd1c9d2ebbdc9cb7d248e1c1600
Rael <santosrafaelpro@gmail.com>
2024-10-22 00:07:09 -0300

    finishing zapzap2

---
+++ Projects/LiveChat/app.py
@@ -0,0 +1,48 @@
+""" 
+vv === Etapas e passo a passo ===
+    
+1. Hashzap
+2. botao de iniciar chat
+3. popup para entrar no chat
+4. quando entrar no chat: (aparece para todo mundo)
+    a mensagem que você entrou no chat
+    o campo e o botão de enviar mensagem
+5. a cada mensagem que você envia (aparece para todo mundo)
+    Nome: Texto da Mensagem
+
+
+produto = {
+    "nome": "iphone",
+    "preço": 6500,
+    "quantidade": 150    
+}
+
+produto["quantidade"]
+ """
+
+# vv === Código ===
+
+from flask import Flask, render_template
+from flask_socketio import SocketIO, send
+
+app = Flask(__name__)
+app.config["SECRET_KEY"] = (
+    "papaodkpfplfvmpbssnvdopkpfv"  # Use SECRET_KEY for session management
+)
+app.debug = True  # Set debug mode correctly
+socketio = SocketIO(app, cors_allowed_origins="*")
+
+
+@socketio.on("message")
+def gerenciar_mensagens(mensagem):
+    print(f"Mensagem: {mensagem}")
+    send(mensagem, broadcast=True)
+
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
+if __name__ == "__main__":
+    socketio.run(app, debug=True, host="localhost")  # Run with debug mode enabled
