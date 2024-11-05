8af60db776af01efbd25bbf628a6eb0f9c375721
Rael <santosrafaelpro@gmail.com>
2024-11-05 09:08:44 -0300

    Updating in pycharm

---
+++ PythonOneDay/Choices and Decisions.py
@@ -0,0 +1,22 @@
+# Condition Statement:
+
+"""
+Not equals: 5 != 2
+Greater than: 5>2
+Smaller than: 2<5
+Greater than or equals to: 5>=2
+5>=5
+Smaller than or equals to: 2 <= 5
+2 <= 2
+"""
+
+'''
+Logical operators: and, or, not
+'''
+
+'''
+== -> Python will check if two values are equal
+= -> Assignment operator
+'''
+
+# if statement
