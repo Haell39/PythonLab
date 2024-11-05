4f4a6dd2e2d319796fdf4ae7c4b1c41c328dd744
Rael <santosrafaelpro@gmail.com>
2024-11-05 09:03:04 -0300

    continuing python studies

---
+++ PythonOneDay/input.py
@@ -0,0 +1,38 @@
+myAge = 24
+myName = 'Rael'
+
+print("Hello World, my name is", myName, "and I am", myAge, "years old")
+
+print("Hello World, my name is %s and I am %d years old." %(myName, myAge*2))
+
+print("Hello World, my name is {} and I am {} years old" .format(myName, myAge))
+
+# Tripple quotes:
+
+print(''' 
+Hello, World!
+My name is James Web and I am 20 years old.
+      ''')
+
+# Escape Characters:
+
+'''
+To print a backslash, use two backslashes: \\ and a tab: \t
+\n gonna start a new line
+'''
+
+print("Hello, World!\nMy name is James Web \\ and I am 20 years old.\t aqui ta o tab")
+
+'''
+To print double quotes: \" and single quotes: \'
+'''
+
+print("Hello, World! My name is \"James Web\" and I am \'20\' years old" )
+
+#OR Just write:
+
+print('Hello, World! My name is "James Web" and I am "20" years old')
+
+# OR:
+
+print("Hello, World! My name is \"James Web and I am \'20 years old")
