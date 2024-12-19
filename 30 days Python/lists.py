cc9017fe06e2dd6606efe40cd627f11379b47111
Rael <santosrafaelpro@gmail.com>
2024-12-19 13:31:12 -0300

    Updating

--- 30 days Python/lists.py
+++ 30 days Python/lists.py
@@ -5,5 +5,19 @@ lst = list()
 emptyList = list()
 print(len(emptyList))
 
-emptList = [] # --> using brackts
+emptList = []  # --> using brackets
 print(len(emptList))
+
+# List with elements
+
+fruits = ["apple", "banana", "cherry", "date", "elderberry", "fig", "grape"]
+vegetables = ["asparagus", "broccoli", "carrot", "daikon", "eggplant", "fennel", "garlic"]
+animal_products = ['milk', 'meat', 'butter', 'yoghurt']      
+web_techs = ['HTML', 'CSS', 'JS', 'React','Redux', 'Node', 'MongDB'] 
+countries = ['Finland', 'Estonia', 'Denmark', 'Sweden', 'Norway']
+
+print('Fruits: ', fruits)
+print('Number of fruits: ', len(fruits))
+
+
+
