34b43faccf7d1878fe8e2b07e7c815dbda094b25
Rael <santosrafaelpro@gmail.com>
2025-01-15 19:29:49 -0300

    Update table1.py

--- New Learnings/table1.py
+++ New Learnings/table1.py
@@ -6,3 +6,8 @@ table = client.get_table(table_ref)
 timestamp_count = len([field for field in table.schema if field.field_type == 'TIMESTAMP']) #run this to find out the number of columns with TIMESTAMP
 
 print(f"Número de colunas com TIMESTAMP: {timestamp_count}")
+
+
+'''OBS'''
+
+# table.schema is a list of schema fields. Each field is represented by a SchemaField object. --> therefore use this to get the schema of the table, schema is english for structure!
