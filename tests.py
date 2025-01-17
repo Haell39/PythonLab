5cb58645c7b5d9a67e374d525aa3840e8eb2cd3d
Rael <santosrafaelpro@gmail.com>
2025-01-17 01:00:23 -0300

    Update tests.py

--- tests.py
+++ tests.py
@@ -1,8 +1,9 @@
-# Obter o esquema da tabela
-table_ref = dataset_ref.table('crime')
-table = client.get_table(table_ref)
 
-# Contar colunas com tipo TIMESTAMP
-timestamp_count = len([field for field in table.schema if field.field_type == 'TIMESTAMP'])
+test = 2
+print(test)
 
-print(f"Número de colunas com TIMESTAMP: {timestamp_count}")
+def testing():
+    msg = "Hello, World!"
+    print(msg)
+
+testing()
